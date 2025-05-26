239a19651203ebbf0ede065a612b05091a0b6206
MxPerrot <hubertbdlb.media@gmail.com>
2025-05-26 03:13:57 +0200

    docs: add comments

--- hello_world.asm
+++ hello_world.asm
@@ -2,16 +2,18 @@ section .data
 	msg db "Hello, world!", 0xA
 	len equ $ - msg
 
-section .text 
+section .text
 	global _start
 
 _start:
+	; write(1, msg, len)
 	mov eax, 4
 	mov ebx, 1
 	mov ecx, msg
 	mov edx, len
 	int 0x80
-	
+
+	; return 0
 	mov eax, 1
 	xor ebx, ebx
 	int 0x80
